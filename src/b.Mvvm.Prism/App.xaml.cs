d9ea5fc76a8a15165ac6140e831f7652bd698a9c
trumpet-developer <83569508+trumpet-developer@users.noreply.github.com>
2022-07-24 14:40:41 +0900

    Mvvm.Prism: ViewModelクラスの実装

--- src/b.Mvvm.Prism/App.xaml.cs
+++ src/b.Mvvm.Prism/App.xaml.cs
@@ -1,8 +1,20 @@
 ﻿using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
 
 namespace b.Mvvm.Prism
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<MainWindow>();
+            return w;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IUserRepository, UserRepository>();
+        }
     }
 }

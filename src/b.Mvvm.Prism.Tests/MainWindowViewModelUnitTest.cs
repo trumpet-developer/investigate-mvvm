90b9b42dfd3499b30076bc29b5ff526f70e4deec
trumpet-developer <83569508+trumpet-developer@users.noreply.github.com>
2022-07-24 14:09:48 +0900

    Mvvm.Prism: Mvvm.Organic ViewModelクラスの流用

---
+++ src/b.Mvvm.Prism.Tests/MainWindowViewModelUnitTest.cs
@@ -0,0 +1,61 @@
+﻿namespace b.Mvvm.Prism
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class MainWindowViewModelUnitTest
+    {
+        [TestMethod]
+        public void コンストラクターで初期化すること()
+        {
+            //// Arrange
+
+            //// Act
+            var actual = new MainWindowViewModel();
+
+            //// Assert
+            Assert.AreEqual(string.Empty, actual.FirstName);
+            Assert.AreEqual(string.Empty, actual.LastName);
+            Assert.AreEqual(string.Empty, actual.FullName);
+        }
+
+        [TestMethod]
+        public void FistNameを設定取得すること()
+        {
+            //// Arrange
+            var target = new MainWindowViewModel();
+
+            //// Act
+            target.FirstName = "first";
+
+            //// Assert
+            Assert.AreEqual("first", target.FirstName);
+        }
+
+        [TestMethod]
+        public void LastNameを設定取得すること()
+        {
+            //// Arrange
+            var target = new MainWindowViewModel();
+
+            //// Act
+            target.LastName = "last";
+
+            //// Assert
+            Assert.AreEqual("last", target.LastName);
+        }
+
+        [TestMethod]
+        public void FullNameを取得すること()
+        {
+            //// Arrange
+            var target = new MainWindowViewModel
+            {
+                FirstName = "first",
+                LastName = "last"
+            };
+
+            //// Assert
+            Assert.AreEqual("first last", target.FullName);
+        }
+    }
+}
---
+++ src/b.Mvvm.Prism/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace b.Mvvm.Prism
+{
+    internal class MainWindowViewModel
+    {
+        private User user = new User();
+
+        public string FirstName
+        {
+            get { return user.FirstName; }
+            set
+            {
+                user.FirstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return user.LastName; }
+            set
+            {
+                user.LastName = value;
+            }
+        }
+
+        public string FullName => $"{FirstName} {LastName}".Trim();
+    }
+}
